d4587c0343a4c515ebb3a32975a35616835ac7c2
Eduardo Montenegro <eduardo-thrash@hotmail.com>
2020-04-04 20:12:38 -0500

    Firts test in cypress.

---
+++ cypress/integration/pruebaCuentas.js
@@ -0,0 +1,18 @@
+describe('cuentas', function () {
+
+    var a = 1 + 1;
+
+    var b = 2;
+
+    it('vamos a ver que se sume bien', function () {
+
+        expect(a == b).to.equal(true);
+
+    })
+
+    it('vamos a ver que reste bien', function () {
+
+        expect(a - b).to.equal(0)
+        
+    })
+})
